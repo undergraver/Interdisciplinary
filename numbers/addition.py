b67a99791b3c49178f00a174f2f2d8cd53e3ce83
Iulian Serbanoiu <undergraver@gmail.com>
2019-05-01 14:10:01 +0300

    Refactor - moved everything into a generic test

--- numbers/addition.py
+++ numbers/addition.py
@@ -1,62 +1,26 @@
 #!/usr/bin/env python3
 
-import number_generator
+from general_test import GeneralTest
 
-class TestAddition():
-    def __init__(self,name,num_challenges,max_value):
-        self.name = name
-        self.num_challenges = num_challenges
-        self.max_value = max_value
+class TestAddition(GeneralTest):
+    def handle_question(self,num1,num2):
 
-    def start(self):
-        text_lines = [ "Hello, {0}".format(self.name),
-                       "Please answer the next {0} questions".format(self.num_challenges)
-                     ]
-        self.display_box_message("\n".join(text_lines))
-        correct_answers = 0
-        num_generator = number_generator.int_pair_generator(0,self.max_value,
-                                                            0,self.max_value)
-        for _ in range(self.num_challenges):
-            num1, num2 = next(num_generator)
-            print("{0}+{1}=".format(num1,num2),end='')
-            try:
-                result = int(input())
-                if result == num1 + num2:
-                    correct_answers+=1
-            except:
-                pass
+        print("{0}+{1}=".format(num1,num2),end='')
+        
+        try:
+            result = int(input())
+            if result == num1 + num2:
+                return True
+        except:
+            pass
 
-        self.display_box_message(
-                "Tudor scored {0} out of {1}".format(
-                correct_answers,
-                self.num_challenges)
-        )
-
-    def display_box_message(self,message):
-        lines = message.split('\n')
-        num_lines = len(lines)
-        max_line_length = max(len(line) for line in lines)
-        delimiter = "+"+max_line_length*"-"+"+"
-        """
-        +----------+
-        |msg line 1|
-        |msg below |
-        +----------+
-        """
-        if num_lines > 0:
-            print(delimiter)
-
-            for line in lines:
-                len_of_line = len(line)
-                print("|"+line+(max_line_length-len_of_line)*' '+"|")
-
-            print(delimiter)
+        return False
 
 
 def main():
     print("Enter your name:",end='')
     name = input()
-    number_of_challenges = 20
+    number_of_challenges = 10
     max_value = 20
     t = TestAddition(name,number_of_challenges,max_value)
     t.start()
---
+++ numbers/general_test.py
@@ -0,0 +1,56 @@
+import number_generator
+
+class GeneralTest():
+    def __init__(self,name,num_challenges,max_value):
+        self.name = name
+        self.num_challenges = num_challenges
+        self.max_value = max_value
+
+    def handle_question(self,num1,num2):
+        """
+        This function handles the operation between num1 and num2.
+        
+        Return True if the answer is correct, False otherwise
+        """
+        raise NotImplementedError
+
+    def start(self):
+        text_lines = [ "Hello, {0}".format(self.name),
+                       "Please answer the next {0} questions".format(self.num_challenges)
+                     ]
+        self.display_box_message("\n".join(text_lines))
+        correct_answers = 0
+        num_generator = number_generator.int_pair_generator(0,self.max_value,
+                                                            0,self.max_value)
+        for _ in range(self.num_challenges):
+            num1, num2 = next(num_generator)
+
+            if self.handle_question(num1,num2):
+                correct_answers += 1
+
+        self.display_box_message(
+                "{0} scored {1} out of {2}".format(
+                self.name,
+                correct_answers,
+                self.num_challenges)
+        )
+
+    def display_box_message(self,message):
+        lines = message.split('\n')
+        num_lines = len(lines)
+        max_line_length = max(len(line) for line in lines)
+        delimiter = "+"+max_line_length*"-"+"+"
+        """
+        +----------+
+        |msg line 1|
+        |msg below |
+        +----------+
+        """
+        if num_lines > 0:
+            print(delimiter)
+
+            for line in lines:
+                len_of_line = len(line)
+                print("|"+line+(max_line_length-len_of_line)*' '+"|")
+
+            print(delimiter)
